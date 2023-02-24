c8d4190ee89e9ddede796fa5572324f2f62fa0a3
Exc3ls10r <111652846+Exc3ls10r@users.noreply.github.com>
2023-02-24 23:27:26 +0200

    task-09-done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtnRef = document.querySelector('button.change-color');
+const textColorValueRef = document.querySelector('span.color');
+
+changeColorBtnRef.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  textColorValueRef.textContent = randomColor;
+  document.body.style.backgroundColor = randomColor;
+});
