28a6396f1140fc91131d0e515c237d915f8081d3
Exc3ls10r <111652846+Exc3ls10r@users.noreply.github.com>
2023-02-24 18:04:21 +0200

    task-02-done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsRefs = ingredients.map(ingredient => {
+  const ingredientRef = document.createElement('li');
+  ingredientRef.textContent = ingredient;
+  ingredientRef.classList.add('item');
+
+  return ingredientRef;
+});
+
+document.querySelector('#ingredients').append(...ingredientsRefs);
