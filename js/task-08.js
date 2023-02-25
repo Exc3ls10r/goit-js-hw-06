33b1a1766795887890db65da2faa977ff258272c
Exc3ls10r <111652846+Exc3ls10r@users.noreply.github.com>
2023-02-25 04:11:35 +0200

    task-08-done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const registrationFormRef = document.querySelector('form.login-form');
 
+registrationFormRef.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const mail = formElements.email.value;
+  const pwd = formElements.password.value;
+
+  if (mail.length === 0 || pwd.length === 0) {
+    window.alert('ВСІ ПОЛЯ ПОВИННІ БУТИ ВВЕДЕНІ !!!')
+  } else {
+    const formData = {
+      email: mail,
+      password: pwd,
+    }
+    console.log(formData);
+    registrationFormRef.reset();
+  }
+});
